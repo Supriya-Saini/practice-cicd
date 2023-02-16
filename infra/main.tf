8cfa15862c78bc329b8f53ccd50aa6d6af855224
Supriya-Saini <ssaini9114@gmail.com>
2023-02-16 10:34:42 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.3"
   backend "azurerm" {
     resource_group_name  = "my-terraform-rg"
-    storage_account_name = "my-terraform-strgacc"
+    storage_account_name = "myterraformstrgacc"
     container_name       = "my-terraform-container"
     key                  = "actions.tfstate"
   }
